4b80954fc50f1f2c3add90920e29b1ddae0b28ad
0xSachinK <sachin@setprotocol.com>
2023-12-09 10:05:57 +0530

    Add initial contracts

---
+++ hardhat.config.ts
@@ -0,0 +1,81 @@
+import * as dotenv from 'dotenv';
+
+import '@typechain/hardhat'
+import 'solidity-coverage'
+import '@nomiclabs/hardhat-ethers'
+import 'hardhat-deploy';
+import '@nomiclabs/hardhat-etherscan';
+import '@nomicfoundation/hardhat-chai-matchers'
+
+import { HardhatUserConfig } from "hardhat/config";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.18",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    hardhat: {
+      allowBlocksWithSameTimestamp: true,
+    },
+    localhost: {
+      allowBlocksWithSameTimestamp: true,
+    },
+    base_staging: {
+      url: "https://developer-access-mainnet.base.org",
+      // @ts-ignore
+      accounts: [
+        `0x${process.env.BASE_DEPLOY_PRIVATE_KEY}`,
+      ],
+      verify: {
+        etherscan: {
+          apiUrl: "https://api.basescan.org",
+          apiKey: process.env.BASESCAN_API_KEY
+        }
+      },
+    },
+    goerli: {
+      url: "https://goerli.infura.io/v3/" + process.env.INFURA_TOKEN,
+      // @ts-ignore
+      accounts: [
+        `0x${process.env.GOERLI_DEPLOY_PRIVATE_KEY}`,
+      ],
+      verify: {
+        etherscan: {
+          apiKey: process.env.ETHERSCAN_API_KEY
+        }
+      },
+    },
+    base: {
+      url: "https://developer-access-mainnet.base.org",
+      // @ts-ignore
+      accounts: [
+        `0x${process.env.BASE_DEPLOY_PRIVATE_KEY}`,
+      ],
+      verify: {
+        etherscan: {
+          apiUrl: "https://api.basescan.org/",
+          apiKey: process.env.BASESCAN_API_KEY
+        }
+      },
+    },
+  },
+  // @ts-ignore
+  typechain: {
+    outDir: "typechain",
+    target: "ethers-v5",
+  },
+};
+
+export default config;
